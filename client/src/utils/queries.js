3ff977c1b19f3e1ff0f04f4c58994d01c69e6946
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-03 17:19:04 -0500

    added apollo/client as a dependency and made queries file to hold all graphql queries in clinet

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import {gql} from '@apollo/client';
+
+export const GET_ME = gql`
+query Me {
+    me {
+      _id
+      username
+      email
+      bookcount
+      savedBooks {
+        _id
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
