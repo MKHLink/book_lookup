c4560bc126c0a81bb57396d3c8b10a7076dd7c3d
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-05 14:07:04 -0500

    filled out the mutation queries in the client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,73 @@
 import {gql} from '@apollo/client';
 
 export const LOGIN_USER=gql`
-
+mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+    token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
 `;
 
 export const ADD_USER=gql`
-
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookcount
+        savedBooks {
+          _id
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK=gql`
-
+mutation SaveBook($input: saveBook) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookcount
+      savedBooks {
+        _id
+        bookId
+        authors
+        description
+        title
+      }
+    }
+  }
 `;
 
 export const REMOVE_BOOK=gql`
-
+mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookcount
+      savedBooks {
+        _id
+        bookId
+        authors
+        description
+        title
+      }
+    }
+  }
 `;
